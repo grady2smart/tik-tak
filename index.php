2ddaf8aa503480f008270f3aae7471c36e90e6c1
grady2smart <84208386+techdech@users.noreply.github.com>
2022-05-15 16:26:27 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+<title>Signer</title>
+</head>
+<body>
+
+<form action="files.php" method="post">
+    <input type="text" name="ipaid" placeholder="iPA" required>
+    <button>Next</button>
+</form>
+
+<center>
+    <nav>
+        <a href="/signer/index.php">
+        <i class="bi bi-upload" style="font-size: 2rem;"></i>
+        </a>
+        <a href="index.html">
+        <i class="bi bi-house" style="font-size: 2rem;"></i>
+        </a>
+        <a href="search.html">
+        <i class="bi bi-search" style="font-size: 2rem;"></i>
+        </a>
+    </nav>
+</center>
+
+</body>
+</html>
